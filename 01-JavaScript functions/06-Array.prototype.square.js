b720a5d0dee8960c04433306d491e14bc93732cf
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-14 22:42:43 +0530

    Update 06-Array.prototype.square.js

--- 01-JavaScript functions/06-Array.prototype.square.js
+++ 01-JavaScript functions/06-Array.prototype.square.js
@@ -1,3 +1,18 @@
+// Implement a custom Array function, Array.prototype.square() which creates a new array with the results of squaring every element
+//within the array the .square() method is called on.
+
+// Examples
+// [-2].square(); // [4]
+// [1, 2, 3, 4].square(); // [1, 4, 9, 16]
+// Notes
+// The original array should not be modified.
+// Assume that the array only contains numbers.
+
 Array.prototype.square = function () {
    return this.map((item)=>item**2);
 };
+
+
+
+
+//No, the .square() method will not modify the original array because it uses the .map() function, which creates a new array and leaves the original array untouched.
