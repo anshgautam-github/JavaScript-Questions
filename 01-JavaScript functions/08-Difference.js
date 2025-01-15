efc2b88758a7b6f56c6fa519b7592db75a58c358
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-15 10:26:41 +0530

    Update 08-Difference.js

--- 01-JavaScript functions/08-Difference.js
+++ 01-JavaScript functions/08-Difference.js
@@ -12,6 +12,22 @@
 // The function should return the original array values if values is empty.
 // difference([1, 2, 3], []); // => [1, 2, 3]
 
+
+//Method-1
 function difference(array, values) {
   return array.filter((val)=>!values.includes(val));
 }
+
+//Method-2 (using set)
+
+// During the filter() operation, we need to check for each element in array whether it exists in values.
+// A Set makes this lookup process faster (O(1)), while searching in an array (using .includes()) would take longer (O(n)).
+
+function difference(array, values) {
+  const valueSet=new Set(values);
+  return array.filter((ele)=>!valueSet.has(ele));
+}
+
+// We apply Set on values because:
+// values is used for lookups, and Set makes lookups fast.
+// Applying a Set on array doesn’t help filter it and might unintentionally remove duplicates from array.
